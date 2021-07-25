f3781ec31add0256594a8f20fbefd71ded96a3b5
Tanjim Hossain <tangimhossain1@gmail.com>
2021-07-24 21:36:14 -0700

    add sound - miniaudio

--- main.cpp
+++ main.cpp
@@ -6,8 +6,11 @@
 #include <iostream>
 #include <math.h>
 #include <random>
+#include <thread>
 #include <tuple>
 #include <vector>
+#define MINIAUDIO_IMPLEMENTATION
+#include "./miniaudio.h"
 
 using namespace std;
 
@@ -553,8 +556,63 @@ void keyboard_key_callback(GLFWwindow *window, int key, int scancode,
   }
 }
 
-int main(void) {
+void data_callback(ma_device *pDevice, void *pOutput, const void *pInput,
+                   ma_uint32 frameCount) {
+  ma_bool32 isLooping = MA_TRUE;
 
+  ma_decoder *pDecoder = (ma_decoder *)pDevice->pUserData;
+  if (pDecoder == NULL) {
+    return;
+  }
+
+  ma_data_source_read_pcm_frames(pDecoder, pOutput, frameCount, NULL,
+                                 isLooping);
+
+  (void)pInput;
+}
+
+int playAudio() {
+  ma_result result;
+  ma_decoder decoder;
+  ma_device_config deviceConfig;
+  ma_device device;
+
+  result = ma_decoder_init_file("Sunny Day-SoundBible.com-2064222612.wav", NULL,
+                                &decoder);
+  if (result != MA_SUCCESS) {
+    return -2;
+  }
+
+  deviceConfig = ma_device_config_init(ma_device_type_playback);
+  deviceConfig.playback.format = decoder.outputFormat;
+  deviceConfig.playback.channels = decoder.outputChannels;
+  deviceConfig.sampleRate = decoder.outputSampleRate;
+  deviceConfig.dataCallback = data_callback;
+  deviceConfig.pUserData = &decoder;
+
+  if (ma_device_init(NULL, &deviceConfig, &device) != MA_SUCCESS) {
+    printf("Failed to open playback device.\n");
+    ma_decoder_uninit(&decoder);
+    return -3;
+  }
+
+  if (ma_device_start(&device) != MA_SUCCESS) {
+    printf("Failed to start playback device.\n");
+    ma_device_uninit(&device);
+    ma_decoder_uninit(&decoder);
+    return -4;
+  }
+
+  getchar();
+
+  ma_device_uninit(&device);
+  ma_decoder_uninit(&decoder);
+
+  return 0;
+}
+
+int main(void) {
+  thread th1(playAudio);
   GLFWwindow *window;
 
   if (!glfwInit())
